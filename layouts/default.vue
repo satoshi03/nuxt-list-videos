edfcaddc74110b41f788721e936440cf4f81b14f
Satoshi Innami <innamisatoshi@gmail.com>
2019-01-13 17:47:33 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-toolbar
+      color="white"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title"/>
+      <v-spacer></v-spacer>
+      <v-autocomplete
+        v-model="model"
+        items="items"
+        color="white"
+        hide-no-data
+        hide-selected
+        item-text="Description"
+        item-value="API"
+        placeholder="検索ワードを入力"
+        prepend-icon="mdi-database-search"
+        class="mx-3"
+        flat
+        return-object
+      ></v-autocomplete>
+      <v-icon>search</v-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; susuru ラーメン検索 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fixed: false,
+        title: 'susuru ラーメン検索'
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center>
+    <v-flex
+      xs12
+      sm8
+      md6>
+      <v-card max-width=600>
+        <v-card-title>激辛ラーメンに一番合うトッピング見つけたかも をすする 蒙古タンメン中本 高田馬場店【飯テロ】 SUSURU TV.第1149回</v-card-title>
+        <v-card-media>
+          <v-img src="https://i.ytimg.com/vi/-MqqVmr9w0w/hqdefault.jpg"></v-img>
+        </v-card-media>
+        <v-card-text>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn
+            color="primary"
+            flat
+            nuxt
+            to="/inspire">再生</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
